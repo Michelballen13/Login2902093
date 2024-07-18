cebd7557b899368ef515c12e5fd8642b99d40db1
Kevin1Paredes <kyparedes1@soy.sena.edu.co>
2024-07-18 17:27:52 -0500

    Cambios

--- app/src/main/java/com/example/login2902093m/screens/SignUpScreen.kt
+++ app/src/main/java/com/example/login2902093m/screens/SignUpScreen.kt
@@ -1,4 +1,37 @@
 package com.example.login2902093m.screens
 
-class SignUpScreen {
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.login2902093m.R
+import com.example.login2902093m.components.NormalTextComponent
+
+@Composable
+fun SignUpScreen() {
+
+
+    Surface(
+        color = Color.White,
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(28.dp)
+            .background(color = Color.White)
+    ) {
+        NormalTextComponent(value = stringResource(id = R.string.hello))
+    } //surface
+} //SignUpScreen
+
+@Preview(showBackground = true)
+@Composable
+fun SignUpScreenPreview() {
+    SignUpScreen()
+
 }
+
--- app/src/main/java/com/example/login2902093m/ui/theme/Color.kt
+++ app/src/main/java/com/example/login2902093m/ui/theme/Color.kt
@@ -2,10 +2,11 @@ package com.example.login2902093m.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Primary = Color(0XFF92A3FD)
+val Secondary = Color(0XFF9DCEFF)
+val TextColor = Color(0XFF1D1617)
+val AccentColor = Color(0XFFC58BF2)
+val GreyColor = Color(0XFF7B6F72)
+val WhiteColor = Color(0XFFFFFFFF)
+
